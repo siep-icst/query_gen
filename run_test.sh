308e5114f38eb8a52b4b6f9d1734d7e239b42470
vainamoinen <weixinluwx@foxmail.com>
2019-04-25 00:22:18 +0800

    line query

--- run_test.sh
+++ run_test.sh
@@ -2,8 +2,6 @@ if [ -d "./query/" ]
 then
     rm -r query
 fi
-git fetch origin
-git merge origin/master
 make clean
 make
 ./run.exe line delaunay_n13_prev.txt query_requirement/line_query_request.txt
