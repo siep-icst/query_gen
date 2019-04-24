2b8ff8d1017ac7ab17fc31ee9b8b2860298840fb
vainamoinen <weixinluwx@foxmail.com>
2019-04-25 00:17:53 +0800

    line query

--- run_test.sh
+++ run_test.sh
@@ -5,4 +5,4 @@ git fetch origin
 git merge origin/master
 make clean
 make
-./run.exe line delaunay_n13_prev.txt query_requirement/line_query_request.txt 
+./run.exe line delaunay_n13_prev.txt query_requirement/line_query_request.txt
