c7272b7654a281bcc090c0636af9443b51698c8b
vainamoinen <weixinluwx@foxmail.com>
2019-04-25 15:08:38 +0800

    clique query

--- run_test.sh
+++ run_test.sh
@@ -4,4 +4,4 @@ then
 fi
 make clean
 make
-./run.exe clique data/delaunay_prev.txt query_requirement/clique_query_request.txt
+./run.exe clique data/delaunay_prev.txt query_requirement/clique_query_requirement.txt
