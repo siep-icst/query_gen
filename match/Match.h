31ef29e5e1ffee1de5824f35e8a741a363b1b527
vainamoinen <weixinluwx@foxmail.com>
2019-04-25 16:27:50 +0800

    ring query

--- match/Match.h
+++ match/Match.h
@@ -20,6 +20,7 @@ public:
 	Match(int queryNodeNum, int queryEdgeNum, int queryNum, Graph* _data);
 	void match(IO& io);
 	void match(std::string _query_dir);
+	void ring_match(std::string _query_dir);
 	~Match();
 
 private:
@@ -30,6 +31,8 @@ private:
     int queryNum;    //generate how many queries in this file
 
     bool isDuplicate(std::vector<int*>&, std::vector<int>&, std::vector<std::pair<int,int>*>&, std::vector<int>&);
+	int get_RandStartId(int _tmp_query_len);
+	int get_RingStartId(int _tmp_query_len);
 };
 
 #endif
