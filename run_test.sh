e945b532d6efcf36cba4ed326cae074753c444ea
vainamoinen <weixinluwx@foxmail.com>
2019-04-25 10:01:35 +0800

    line query

--- run_test.sh
+++ run_test.sh
@@ -4,4 +4,4 @@ then
 fi
 make clean
 make
-./run.exe line delaunay_n13_prev.txt query_requirement/line_query_request.txt
+./run.exe line data/delaunay_prev.txt query_requirement/line_query_request.txt
