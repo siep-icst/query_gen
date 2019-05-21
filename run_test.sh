4118de773dc732a0c3e1c7b80db1c25711344f6e
vainamoinen <weixinluwx@foxmail.com>
2019-05-21 15:38:25 +0800

    change format of query file

--- run_test.sh
+++ run_test.sh
@@ -1,2 +1,2 @@
 
-./run.exe ring dataset/yeast_new.igraph query_requirement/ring_query_requirement.txt test_yeast_ring_query
+./run.exe line dataset/yeast_new.igraph query_requirement/line_query_requirement.txt test_yeast_line_query
